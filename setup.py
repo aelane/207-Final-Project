4b56c46519000721db621ecb857454e18c511adb
aelane <aelane@berkeley.edu>
2018-07-14 10:51:09 -0500

    Pushing up folder/file structure from cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='W207.3 Summer 2018 session n for UC Berkeley MIDS program. Applying ML & Feature Engineering Techniques to realdatasets to build classifiers and draw insights',
+    author='CoolKids',
+    license='MIT',
+)
